9427b942c6f6e22014f86a44de23b436325f80e2
Eric Riviere <eric.riviere.ponsa@gmail.com>
2023-05-25 23:33:26 +0200

    comentarios y mejoras

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let command;
 
-//General function
+//Cart function
 function cart() {
   alert(
     'Enter the products you want to add to your caart: \n\n- Add the name of the product EXACTLY as displayed in the web.\n - Once you have added all the products you want to buy, enter "TOTAL" to know the total ammount of your purchase.'
@@ -8,14 +8,14 @@ function cart() {
 
   let list = "";
   let total = 0;
-  //Cycle to add products until "TOTAL" command indicates to calculate the cart total amount
+  //Cycle to add products until "TOTAL" command is introduced to calculate the cart total amount.
   while (command != "TOTAL") {
     command = prompt(
       'Enter a product to your cart\n- Or enter "TOTAL" to know the total ammount'
     );
 
-    //Switch with all product cases to be introduced
-    //Every case includes an addition to cart list, an alert to confirm the product has been added and the price addition to total amount
+    //Switch with all product cases to be introduced as commands.
+    //Every case includes an addition to cart list, an alert to confirm the product has been added and the price addition to total amount.
     switch (command) {
       case "MAAP x 100% Hypercraft":
         list += "-" + command + " (305 $AUD)\n";
@@ -112,14 +112,15 @@ function cart() {
         alert(command + " added to cart");
         total += 355;
         break;
-      //The case TOTAL first
+      //The case TOTAL first displays all the products on your shopping cart.
       case "TOTAL":
         alert(
           "These are the products on your cart:\n\n" +
             list +
-            "\nProcessing the total ammount..."
+            "\nProcessing the total amount..."
         );
         break;
+      //If a product is not "EXACTLY" witten as displayed in the web, this default alert let you know that the product does´t exist.
       default:
         alert(
           "The product " +
@@ -131,39 +132,54 @@ function cart() {
     }
   }
 
+  //If the user enters the "TOTAL" command, an alert is trigged with all the products on your shopping cart and the total amout.
   if (command == "TOTAL") {
     alert(
       "Products on your cart:\n\n" +
         list +
         "\n" +
-        "The total ammount of your order is " +
+        "The total amount of your order is " +
         total +
         " $AUD"
     );
   }
 
-  let discount = prompt("Add a discount cupon if you have one:");
+  //This prompt let you introduce the discount coupon to reduce 20% on the total amount.
+  let discount = prompt("Add a discount coupon if you have one:");
 
-  if (discount === "SPECIAL20MAAP" && total >= 250) {
+  //If you introduce the coupon displayed on the offcanvas correctly and the total amount is > to 250$AUD (condition stated on the offcanvas),
+  //this conditional applyes a 20% discount to the total amount and displays: shopping cart list, previous total amount, the amount that
+  //represents 20% of your total amount, and the final price with the discount applyed.
+  if (discount === "SPECIAL20MAAP" && total > 250) {
     let discountAmount = (total * 20) / 100;
     totalDiscount = total - discountAmount;
     confirm(
       "Products on your cart:\n" +
         list +
         "\n" +
-        "Previous price: " +
+        "Previous total amount: " +
         total +
         " $AUD" +
         "\n  (20% discount: - " +
         discountAmount +
         " $AUD)" +
-        "\nPrice with discount: " +
+        "\nFinal price (with discount): " +
         totalDiscount +
         " $AUD"
     );
-  } else if (discount === "SPECIAL20MAAP" && total < 250) {
+    //If you enter the coupon right but the total amount is < 250$AUD (condition stated on the offcanvas for the coupon to be applied),
+    //this alert clarifies the motive why the 20% was not applied.
+    /*
+    This condition can be tested by building the following shopping cart list:
+    Evade Bottle
+    Emerge Pro Air Sock
+    Knee Warmers
+    Arm Warmers
+    ADL 2023 Sock
+    */
+  } else if (discount === "SPECIAL20MAAP" && total <= 250) {
     alert(
-      "The total ammount is not enough to apply discount (min 250$AUD)\n\n" +
+      "The total amount is not enough to apply discount (orders over 250$AUD)\n\n" +
         "Products on your cart:\n" +
         list +
         "\n" +
@@ -171,9 +187,10 @@ function cart() {
         total +
         " $AUD"
     );
+    //This alert indicates that the coupon introduced is not correct.
   } else {
     alert(
-      "The discount cupon " +
+      "The discount coupon " +
         '"' +
         discount +
         '"' +
